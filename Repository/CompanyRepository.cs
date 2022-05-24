ff92edd3edea0001ba333090fc09acf6910c8bc2
lvgutierrez02 <71912902+lvgutierrez02@users.noreply.github.com>
2022-05-24 08:44:43 -0500

    hoy

--- Repository/CompanyRepository.cs
+++ Repository/CompanyRepository.cs
@@ -16,5 +16,7 @@ namespace Repository
             base(repositoryContext)
         {
         }
+
+        public IEnumerable<Company> GetAllCompanies(bool trackChanges) => FindAll(trackChanges).OrderBy(c => c.Name).ToList();
     }
 }
