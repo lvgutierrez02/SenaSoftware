0635ce83fab7280fd71b05d724e11e83b166f7ed
lvgutierrez02 <71912902+lvgutierrez02@users.noreply.github.com>
2022-05-25 09:02:49 -0500

    hoy

--- Repository/EmployeeRepository.cs
+++ Repository/EmployeeRepository.cs
@@ -21,5 +21,9 @@ namespace Repository
              FindByCondition(e => e.CompanyId.Equals(companyId), trackChanges)
              .OrderBy(e => e.Name);
 
+         public Employee GetEmployee(Guid companyId, Guid id, bool trackChanges) =>FindByCondition(e => e.CompanyId.Equals(companyId) && e.Id.Equals(id),
+            trackChanges).SingleOrDefault();
+
+
     }
 }
--- Seguimiento.API/Controllers/EmployeesController.cs
+++ Seguimiento.API/Controllers/EmployeesController.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using Contracts;
+using Entities.DTO;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Seguimiento.API.Controllers
@@ -21,8 +22,9 @@ namespace Seguimiento.API.Controllers
 
 
         [HttpGet]
-        public IActionResult GetEmployeesForCompany(Guid companyId) //tenemos el parámetro parámetro companyId en nuestra acción y este parámetro será mapeado
-                                                                    //desde la ruta principal.Por esa razón, no lo colocamos en el atributo [HttpGet]
+        public IActionResult GetEmployeesForCompany(Guid companyId)//tenemos el parámetro parámetro companyId en nuestra acción y este parámetro será mapeado
+                                                                   //desde la ruta principal.Por esa razón, no lo colocamos en el atributo [HttpGet]
+
         {
             var company = _repository.Company.GetCompany(companyId, trackChanges: false);
             if (company == null)
@@ -32,8 +34,30 @@ namespace Seguimiento.API.Controllers
             }
             var employeesFromDb = _repository.Employee.GetEmployees(companyId,
            trackChanges: false);
-            return Ok(employeesFromDb);
+            var employeesDto = _mapper.Map<IEnumerable<EmployeeDto>>(employeesFromDb);
+            return Ok(employeesDto);
         }
+
+        [HttpGet("{id}")]
+        public IActionResult GetEmployeeForCompany(Guid companyId, Guid id)
+        {
+            var company = _repository.Company.GetCompany(companyId, trackChanges: false);
+            if (company == null)
+            {
+                _logger.LogInfo($"Company with id: {companyId} doesn't exist in the database.");
+            return NotFound();
+            }
+            var employeeDb = _repository.Employee.GetEmployee(companyId, id, trackChanges:
+           false);
+            if (employeeDb == null)
+            {
+                _logger.LogInfo($"Employee with id: {id} doesn't exist in the database.");
+                return NotFound();
+            }
+            var employee = _mapper.Map<EmployeeDto>(employeeDb);
+            return Ok(employee);
+        }
+
 
     }
 
